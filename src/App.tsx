b0347de415f6d7bb2eb1a36f844d0b81612cac85
Mohammed Nasif <m.khaled100@ymail.com>
2022-12-03 12:31:20 +0200

    update tsConfig and remove app.tsx content

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis nam itaque quia ipsa rerum voluptate enim tempore sit quibusdam vitae?
+				Molestias nesciunt et sint animi, ut voluptatem totam blanditiis atque.
+			</header>
+		</div>
+	);
 }
 
 export default App;
